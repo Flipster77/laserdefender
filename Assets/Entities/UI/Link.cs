1087fb41950aa1e6cff8fddba3bda113586c5f64
Flipster77 <Flipster77@users.noreply.github.com>
2016-08-16 23:01:03 +0930

    Added credits and tweaked UI scaling.

---
+++ Assets/Entities/UI/Link.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Link : MonoBehaviour {
+
+    public string LinkAddress;
+
+    void Start() {
+        if (string.IsNullOrEmpty(LinkAddress)) {
+            Debug.LogError("Link has no specified address.");
+        }
+    }
+
+	public void OpenLink() {
+        Application.OpenURL(LinkAddress);
+    }
+}
---
+++ Assets/Entities/UI/PauseMenu.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    private GameObject[] objectsToPause;
+    private GameObject[] pauseMenuObjects;
+
+    private bool gamePaused = false;
+
+    // Use this for initialization
+	void Start () {
+        pauseMenuObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        HidePauseMenu();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            objectsToPause = GameObject.FindGameObjectsWithTag("AffectedByPause");
+
+            if (gamePaused) {
+                ResumeGame();
+            } else {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame() {
+        Debug.Log("Game paused");
+
+        foreach (GameObject obj in objectsToPause) {
+            obj.SendMessage("OnPauseGame", SendMessageOptions.DontRequireReceiver);
+        }
+
+        ShowPauseMenu();
+        gamePaused = true;
+    }
+
+    public void ResumeGame() {
+        Debug.Log("Game resumed");
+
+        foreach (GameObject obj in objectsToPause) {
+            obj.SendMessage("OnResumeGame", SendMessageOptions.DontRequireReceiver);
+        }
+
+        HidePauseMenu();
+        gamePaused = false;
+    }
+
+    private void ShowPauseMenu() {
+
+        foreach (GameObject obj in pauseMenuObjects) {
+            obj.SetActive(true);
+        }
+    }
+
+    private void HidePauseMenu() {
+
+        foreach (GameObject obj in pauseMenuObjects) {
+            obj.SetActive(false);
+        }
+    }
+}
